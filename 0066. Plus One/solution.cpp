9ce258205f075c310880d506861bfac2640346ab
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-09-03 18:04:49 +0530

    Create solution.cpp

---
+++ 0066. Plus One/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;   // just increment and return
+                return digits;
+            }
+            digits[i] = 0;  // carry over
+        }
+        
+        // If we reach here, all digits were 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};

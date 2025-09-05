c1f81c6dfcb94d00ff03239cddf0c6d1ee5fa9ad
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-09-05 13:05:02 +0530

    Create solution.c

---
+++ 0991. Broken Calculator/solution.c
@@ -0,0 +1,12 @@
+int brokenCalc(int startValue, int target) {
+    int operations = 0;
+    while (target > startValue) {
+        if (target % 2 == 0) {
+            target /= 2;
+        } else {
+            target += 1;
+        }
+        operations++;
+    }
+    return operations + (startValue - target);
+}

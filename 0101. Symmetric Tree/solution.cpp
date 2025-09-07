d37c9fad878b13f1234bae143dc388cdbf6952b2
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-09-07 16:56:40 +0530

    Create solution.cpp

---
+++ 0101. Symmetric Tree/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+
+        while (!q.empty()) {
+            TreeNode* t1 = q.front(); q.pop();
+            TreeNode* t2 = q.front(); q.pop();
+
+            if (!t1 && !t2) continue;
+            if (!t1 || !t2) return false;
+            if (t1->val != t2->val) return false;
+
+            // enqueue children in mirrored order
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};

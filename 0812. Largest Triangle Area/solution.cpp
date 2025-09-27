600fbd4dc177b51e51ebad4793f5fe2308525c56
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-09-27 14:09:06 +0530

    Create solution.cpp

---
+++ 0812. Largest Triangle Area/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        int n = points.size();
+        double maxArea = 0.0;
+
+        // Check every combination of 3 points
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    double x1 = points[i][0], y1 = points[i][1];
+                    double x2 = points[j][0], y2 = points[j][1];
+                    double x3 = points[k][0], y3 = points[k][1];
+
+                    // Shoelace formula
+                    double area = fabs(x1 * (y2 - y3) +
+                                       x2 * (y3 - y1) +
+                                       x3 * (y1 - y2)) / 2.0;
+
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};

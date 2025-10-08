c8dd52e9d875ac74933e2779936638e6c399b45d
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-10-08 23:39:44 +0530

    Create solution.cpp

---
+++ 1528. Shuffle String/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string result(s.size(), ' ');
+        
+        for (int i = 0; i < s.size(); i++) {
+            result[indices[i]] = s[i];
+        }
+        
+        return result;
+    }
+};

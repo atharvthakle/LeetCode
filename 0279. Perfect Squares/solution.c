288621199aab406b78162d1b694f036725a0171c
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-11-09 22:49:52 +0530

    Create solution.c

---
+++ 0279. Perfect Squares/solution.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <limits.h>
+
+int numSquares(int n) {
+    int dp[n + 1];
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = INT_MAX;
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = fmin(dp[i], dp[i - j * j] + 1);
+        }
+    }
+
+    return dp[n];
+}

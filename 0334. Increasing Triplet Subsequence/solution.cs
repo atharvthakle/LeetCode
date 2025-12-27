98ffccec314a21eb84eb67d907ec432332b18d66
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2025-12-27 13:07:33 +0530

    Create solution.cs

---
+++ 0334. Increasing Triplet Subsequence/solution.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IncreasingTriplet(int[] nums) {
+        int first = int.MaxValue;
+        int second = int.MaxValue;
+
+        foreach (int num in nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                // Found third number greater than first and second
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

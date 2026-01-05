7b9d9cb50d5b76438e1b365b824f03899a0cf552
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-01-05 16:03:29 +0530

    Create solution.cs

---
+++ 0376. Wiggle Subsequence/solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int WiggleMaxLength(int[] nums) {
+        if (nums.Length < 2) return nums.Length;
+
+        int up = 1;
+        int down = 1;
+
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+
+        return Math.Max(up, down);
+    }
+}

18d72daa6658501b10bf7a1f12e4ca1b23fccfe8
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-01-13 14:22:51 +0530

    Create solution.go

---
+++ 0389. Find the Difference/solution.go
@@ -0,0 +1,13 @@
+func findTheDifference(s string, t string) byte {
+    var res byte = 0
+    
+    for i := 0; i < len(s); i++ {
+        res ^= s[i]
+    }
+    
+    for i := 0; i < len(t); i++ {
+        res ^= t[i]
+    }
+    
+    return res
+}

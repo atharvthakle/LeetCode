f66d1f33923ee0945dd76d6c2fcfbac19fce2c52
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-02-14 12:59:40 +0530

    Update solution.cs

--- 0799. Champagne Tower/solution.cs
+++ 0799. Champagne Tower/solution.cs
@@ -1,24 +1,22 @@
-func champagneTower(poured int, query_row int, query_glass int) float64 {
-    // max rows needed is 100
-    dp := make([][]float64, query_row+2)
-    for i := range dp {
-        dp[i] = make([]float64, i+1)
-    }
+public class Solution {
+    public double ChampagneTower(int poured, int query_row, int query_glass) {
+        double[][] dp = new double[101][];
+        for (int i = 0; i < 101; i++) {
+            dp[i] = new double[101];
+        }
 
-    dp[0][0] = float64(poured)
+        dp[0][0] = poured;
 
-    for r := 0; r <= query_row; r++ {
-        for c := 0; c <= r; c++ {
-            if dp[r][c] > 1.0 {
-                overflow := (dp[r][c] - 1.0) / 2.0
-                dp[r+1][c] += overflow
-                dp[r+1][c+1] += overflow
+        for (int i = 0; i < query_row; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[i][j] > 1.0) {
+                    double excess = (dp[i][j] - 1.0) / 2.0;
+                    dp[i+1][j] += excess;
+                    dp[i+1][j+1] += excess;
+                }
             }
         }
-    }
 
-    if dp[query_row][query_glass] > 1.0 {
-        return 1.0
+        return Math.Min(1.0, dp[query_row][query_glass]);
     }
-    return dp[query_row][query_glass]
 }

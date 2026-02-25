e0fd212072295143f34855165b9f6ea179828886
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-02-25 12:54:31 +0530

    Create solution.go

---
+++ 1356. Sort Integers by The Number of 1 Bits/solution.go
@@ -0,0 +1,18 @@
+import (
+    "sort"
+    "math/bits"
+)
+
+func sortByBits(arr []int) []int {
+    sort.Slice(arr, func(i, j int) bool {
+        countI := bits.OnesCount(uint(arr[i]))
+        countJ := bits.OnesCount(uint(arr[j]))
+        
+        if countI == countJ {
+            return arr[i] < arr[j]
+        }
+        return countI < countJ
+    })
+    
+    return arr
+}

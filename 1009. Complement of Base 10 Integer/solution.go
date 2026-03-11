68caa072a7fa534fda453b8ec6de3ac014b2f69f
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-03-11 13:16:50 +0530

    Create solution.go

---
+++ 1009. Complement of Base 10 Integer/solution.go
@@ -0,0 +1,15 @@
+func bitwiseComplement(n int) int {
+    if n == 0 {
+        return 1
+    }
+
+    mask := 0
+    temp := n
+
+    for temp > 0 {
+        mask = (mask << 1) | 1
+        temp >>= 1
+    }
+
+    return n ^ mask
+}

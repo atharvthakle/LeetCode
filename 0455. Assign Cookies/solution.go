b2e09f5772de28dc1b3fd61c9058f604ea1106f9
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-03-15 14:54:29 +0530

    Implement function to assign cookies to children

---
+++ 0455. Assign Cookies/solution.go
@@ -0,0 +1,21 @@
+import "sort"
+
+func findContentChildren(g []int, s []int) int {
+    sort.Ints(g)
+    sort.Ints(s)
+
+    i, j := 0, 0
+    count := 0
+
+    for i < len(g) && j < len(s) {
+        if s[j] >= g[i] {
+            count++
+            i++
+            j++
+        } else {
+            j++
+        }
+    }
+
+    return count
+}

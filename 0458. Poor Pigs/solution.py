be47d0f20f5fecf006ba0b68b2f42ad55436dda3
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-03-15 15:33:58 +0530

    Delete 0458. Poor Pigs/solution.py

--- 0458. Poor Pigs/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        rounds = minutesToTest // minutesToDie
-        pigs = 0
-        
-        while (rounds + 1) ** pigs < buckets:
-            pigs += 1
-            
-        return pigs

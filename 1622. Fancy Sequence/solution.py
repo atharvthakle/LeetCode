ab2396ab0dc33206618b841e3be355f80d1c080c
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-03-15 14:51:41 +0530

    Create solution.py

---
+++ 1622. Fancy Sequence/solution.py
@@ -0,0 +1,27 @@
+class Fancy:
+
+    MOD = 10**9 + 7
+
+    def __init__(self):
+        self.seq = []
+        self.mul = 1
+        self.add = 0
+
+    def append(self, val: int) -> None:
+        # normalize the value before storing
+        inv = pow(self.mul, -1, self.MOD)
+        val = (val - self.add) % self.MOD
+        val = (val * inv) % self.MOD
+        self.seq.append(val)
+
+    def addAll(self, inc: int) -> None:
+        self.add = (self.add + inc) % self.MOD
+
+    def multAll(self, m: int) -> None:
+        self.mul = (self.mul * m) % self.MOD
+        self.add = (self.add * m) % self.MOD
+
+    def getIndex(self, idx: int) -> int:
+        if idx >= len(self.seq):
+            return -1
+        return (self.seq[idx] * self.mul + self.add) % self.MOD

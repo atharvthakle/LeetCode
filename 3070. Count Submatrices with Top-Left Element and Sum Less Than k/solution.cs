c66973fb49ca33d3e39eed0e7c9faff344a5b11d
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-03-18 11:20:41 +0530

    Create solution.cs

---
+++ 3070. Count Submatrices with Top-Left Element and Sum Less Than k/solution.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int CountSubmatrices(int[][] grid, int k) {
+        int m = grid.Length;
+        int n = grid[0].Length;
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i > 0)
+                    grid[i][j] += grid[i - 1][j];
+
+                if (j > 0)
+                    grid[i][j] += grid[i][j - 1];
+
+                if (i > 0 && j > 0)
+                    grid[i][j] -= grid[i - 1][j - 1];
+
+                if (grid[i][j] <= k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}

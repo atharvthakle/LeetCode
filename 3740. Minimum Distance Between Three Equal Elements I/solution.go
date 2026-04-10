32bf1a9e38421d169a866a3d1faacc5226db38c4
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-04-10 14:13:18 +0530

    Update solution.go

--- 3740. Minimum Distance Between Three Equal Elements I/solution.go
+++ 3740. Minimum Distance Between Three Equal Elements I/solution.go
@@ -1,1 +1,28 @@
+func minimumDistance(nums []int) int {
+    // map value -> list of indices
+    mp := make(map[int][]int)
 
+    for i, v := range nums {
+        mp[v] = append(mp[v], i)
+    }
+
+    res := int(1e9)
+
+    for _, pos := range mp {
+        if len(pos) < 3 {
+            continue
+        }
+
+        for i := 0; i+2 < len(pos); i++ {
+            dist := 2 * (pos[i+2] - pos[i])
+            if dist < res {
+                res = dist
+            }
+        }
+    }
+
+    if res == int(1e9) {
+        return -1
+    }
+    return res
+}

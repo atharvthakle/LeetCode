a0e96d1568158cfc64eec199899d6a02d915390e
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-05-07 12:34:54 +0530

    Create solution.java

---
+++ 3660. Jump Game IX/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] maxValue(int[] nums) {
+        int n = nums.length;
+
+        int[] prefixMax = new int[n];
+        int[] suffixMin = new int[n];
+
+        prefixMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+        }
+
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+        }
+
+        int[] ans = new int[n];
+
+        int start = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            // split components when NO inversion exists across boundary
+            if (prefixMax[i] <= suffixMin[i + 1]) {
+
+                int componentMax = prefixMax[i];
+
+                for (int j = start; j <= i; j++) {
+                    ans[j] = componentMax;
+                }
+
+                start = i + 1;
+            }
+        }
+
+        // last component
+        int componentMax = prefixMax[n - 1];
+
+        for (int j = start; j < n; j++) {
+            ans[j] = componentMax;
+        }
+
+        return ans;
+    }
+}

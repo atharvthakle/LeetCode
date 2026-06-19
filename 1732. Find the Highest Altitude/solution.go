9e16583c48d84d0a458f555beab1dba9d61bb440
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-06-19 09:25:15 +0530

    Create solution.go

---
+++ 1732. Find the Highest Altitude/solution.go
@@ -0,0 +1,13 @@
+func largestAltitude(gain []int) int {
+	altitude := 0
+	maxAltitude := 0
+
+	for _, g := range gain {
+		altitude += g
+		if altitude > maxAltitude {
+			maxAltitude = altitude
+		}
+	}
+
+	return maxAltitude
+}
